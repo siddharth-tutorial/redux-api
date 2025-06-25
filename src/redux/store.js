8db68ed71e5247de52690047b85a1560f32f0892
siddharth-tutorial <ggvanecha@gmail.com>
2025-06-25 12:07:28 +0530

    Redux Page

---
+++ src/redux/store.js
@@ -0,0 +1,13 @@
+// src/redux/store.js
+import { configureStore } from "@reduxjs/toolkit";
+
+import todoReducer from "./slice/todoSlice"
+
+const store = configureStore({
+  reducer: {
+
+    todo :todoReducer,
+  },
+});
+
+export default store;
